41197d171f3e37d7bb5024816392c3bddc79cacc
IbnuHs <ibnuhs30@gmail.com>
2026-02-04 01:17:50 +0900

    edit config db

--- db/db.js
+++ db/db.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: process.env.USER_DB || "root",
+  password: process.env.PASSWORD_DB || "",
   database: "tester",
   waitForConnections: true,
 });
